cc394acf8883594d4bc8fe7ffc03a920a8dc3f2e
yassmine112 <65820404+yassmine112@users.noreply.github.com>
2020-05-24 23:29:50 +0200

    create categorydata

---
+++ lib/screens/categorydata.dart
@@ -0,0 +1,20 @@
+import '../category/categorymodel.dart';
+
+
+final categories = [
+  Category(
+    numberOfItems: 43,
+    imagePath : ("assets/images/burger.png"),
+    categoryName: "Sandwich"
+  ),
+
+  Category(
+    numberOfItems: 23,
+    imagePath:("assets/images/pizza.png"),
+    categoryName: "pizza"
+  ),
+
+    
+
+    
+];
