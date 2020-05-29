8c8c2aeadad998e504ae444365488a26fdc87c34
norhan ibrahim <61943795+norhan42@users.noreply.github.com>
2020-05-29 19:59:20 +0200

    Create fooddata.dart

---
+++ lib/screens/fooddata.dart
@@ -0,0 +1,36 @@
+import 'foodmodel.dart';
+final foods=[
+  Food(
+    id: "1",
+    name: "Hambuger",
+    imagepath: ("assets/images/bur1.jpg"),
+    category: "1",
+    price: 70.0,
+    discount: 22.0,
+    ratings: 99.0,
+
+  ),
+  /*Food(
+    id: "2",
+    name: "Chicken Pizza",
+    imagepath: ("assets/images/piz1.jpg"),
+    category: "2",
+    price: 60.0,
+    discount: 20.0,
+    ratings: 99.0,
+
+  ),
+  Food(
+    id: "3",
+    name: "SUPREME SANDWICH",
+    imagepath:("assets/images/bur2.jpg"),
+    category: "1",
+    price: 80.0,
+    discount: 22.0,
+    ratings: 99.0,
+
+  ),*/
+   
+  
+  
+];
