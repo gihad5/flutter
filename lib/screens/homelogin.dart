6e389b8c5e03f90ac8979caa84d8f378b246d713
yassmine112 <65820404+yassmine112@users.noreply.github.com>
2020-06-03 19:55:17 +0200

    Update homelogin.dart

--- lib/screens/homelogin.dart
+++ lib/screens/homelogin.dart
@@ -1,40 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:ordering_food/scoped_model/food_model.dart';
+import 'category.dart';
+import 'searchfield.dart';
+import 'food.dart';
+import 'fooddata.dart';
+import 'foodmodel.dart';
 
-import 'login.dart';
-class HomeloginScreen extends StatelessWidget{
-   @override
-  Widget build(BuildContext context){
-    
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.orange[500],
-        title: Text('FOOD',
-                    style:TextStyle(
-                      fontFamily:'Lobster',
-                      fontSize:40.0,
-                      fontWeight:FontWeight.bold,
-                      color:Colors.white,
-                    ) ,
-                    ),
-      
-       actions: <Widget>[
-          RaisedButton(
-            child: Text('Log Out',style: TextStyle(fontSize: 18,color: Colors.white),),
-            color: Colors.orange,
-            onPressed: () async{
-          //await FirebaseAuth.instance.signOut();
-Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+ class HomeLoginScreen extends StatefulWidget {
+   final FoodModel foodModel;
+   HomeLoginScreen(this.foodModel);
 
-            }
-      ),
-       ],
-      ),
-      
-    
-    body: Container(
-child: Text ('Weclome To Your Home'),
-    ),
-
-    );
+    @override
+    _HomeScreenState createState() => _HomeScreenState();
   }
   
+  class _HomeScreenState extends State<HomeLoginScreen> {
+//List<Food> _foods=foods;
+@override
+void initState(){
+  widget.foodModel.fetchFoods();
+  super.initState();
+}
+    final textStyle=TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,);
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: ListView(
+          padding: EdgeInsets.only(top:50.0,left:20.0,right:20.0),
+          children :<Widget>[
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+              
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("What Would" ,style: textStyle,),
+                  Text("You like to eat", style: textStyle,)
+                ],
+              ),
+              Icon(Icons.notifications_none,size: 40,color: Theme.of(context).primaryColor,),
+              
+            ],
+            ),
+            FoodCategory(),
+            SearchField(),
+            SizedBox(height:20.0,),
+            Column(children: widget.foodModel.foods.map(_buildFoodItems).toList(),
+            
+            )
+          ],
+
+        ),
+      );
+
+    }
+    Widget _buildFoodItems(Food food){
+      var boughtFoods = BoughtFoods(
+          id:food.id,
+          name: food.name,
+          category: food.category,
+         imagepath: ("assets/images/bur1.jpg"),
+          discount: food.discount,
+          price: food.price,
+          ratings: food.ratings,
+        );
+      return Container(
+        margin: EdgeInsets.only(bottom:20.0),
+        child: boughtFoods,
+
+      );
+    }
   }
