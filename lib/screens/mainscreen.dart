9dbe7cb624f61d2d1d2de3c354fe11da4cf6dcca
norhan ibrahim <61943795+norhan42@users.noreply.github.com>
2020-06-05 04:29:04 +0200

    Update mainscreen.dart

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,33 +1,34 @@
 import'package:flutter/material.dart' ;
+import 'package:ordering_food/screens/deleteorder.dart';
 import 'homelogin.dart';
 import 'orderpage.dart';
+import 'deleteorder.dart';
+import 'confirmorder.dart';
 class MainScreen extends StatefulWidget{
-  final FoodModel foodModel;
-  MainScreen({
-    this.foodModel
-
-  });
 @override
 _MainScreenState createState() => _MainScreenState();
 }
 class _MainScreenState extends State<MainScreen>{
   int currentTapIndex=0;
-  //List<Widget> scr;
-  HomeLoginScreen homeloginScreen;
-  OrderPage orderPage;
   List<Widget> scr;
-
+  HomeLoginScreen homeScreen;
+  OrderPage orderPage;
+  DeleteOrder deleteOrder;
+  ConfirmOrder confirmOrder;
+  
   Widget currentpage;
   @override
   void initState() {
-    //widget.foodModel.fetchFoods();
-   
-    homeloginScreen = HomeLoginScreen(widget.foodModel);
+    
+    super.initState();
+    homeScreen = HomeLoginScreen();
     orderPage = OrderPage();
-    scr=[homeloginScreen,orderPage];
+    deleteOrder = DeleteOrder();
+    confirmOrder =  ConfirmOrder();
 
-    currentpage =homeloginScreen;
-     super.initState();
+    scr=[homeScreen,orderPage,deleteOrder,confirmOrder];
+
+    currentpage =homeScreen;
   }
   @override
   Widget build(BuildContext context){
@@ -49,14 +50,20 @@ class _MainScreenState extends State<MainScreen>{
               
               ),
               BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),
-              title: Text("Your Cart"))
+              title: Text("Your Cart")),
+               BottomNavigationBarItem(icon: Icon(Icons.delete),
+              title: Text("delete")),
+               BottomNavigationBarItem(icon: Icon(Icons.confirmation_number),
+              title: Text("confirm"))
 
+            
             ]
 
       ),
       body: currentpage,
-      
 
     );
   }
 }
+
+
