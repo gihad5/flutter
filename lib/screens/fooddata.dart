1f0e1173715270c4f590ebfdc62ffd40e9c47d6c
norhan ibrahim <61943795+norhan42@users.noreply.github.com>
2020-06-05 04:26:36 +0200

    Update fooddata.dart

--- lib/screens/fooddata.dart
+++ lib/screens/fooddata.dart
@@ -3,17 +3,17 @@ final foods=[
   Food(
     id: "1",
     name: "Hambuger",
-    imagepath: ("assets/images/bur1.jpg"),
+    imagepath: ("bur1.jpg"),
     category: "1",
     price: 70.0,
     discount: 22.0,
     ratings: 99.0,
 
   ),
-  /*Food(
+  Food(
     id: "2",
     name: "Chicken Pizza",
-    imagepath: ("assets/images/piz1.jpg"),
+    imagepath: ("piz1.jpg"),
     category: "2",
     price: 60.0,
     discount: 20.0,
@@ -23,13 +23,13 @@ final foods=[
   Food(
     id: "3",
     name: "SUPREME SANDWICH",
-    imagepath:("assets/images/bur2.jpg"),
+    imagepath:("bur2.jpg"),
     category: "1",
     price: 80.0,
     discount: 22.0,
     ratings: 99.0,
 
-  ),*/
+  ),
    
   
   
